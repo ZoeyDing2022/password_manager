e8a3e7056b792d04cf2a5ab95ab73fd2ad9262cd
ZoeyDing2022 <dzhaolan@hotmail.com>
2022-10-31 23:03:11 -0700

    Json file is used to store the information. search button is added so that you can search the email used to register and password of the website.

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-#Password Generator Project
+# Password Generator Project
 def generate_password():
-
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -18,41 +21,69 @@ def generate_password():
     password_list = []
 
     for char in range(nr_letters):
-      password_list.append(random.choice(letters))
+        password_list.append(random.choice(letters))
 
     for char in range(nr_symbols):
-      password_list += random.choice(symbols)
+        password_list += random.choice(symbols)
 
     for char in range(nr_numbers):
-      password_list += random.choice(numbers)
+        password_list += random.choice(numbers)
 
     random.shuffle(password_list)
 
     password = "".join(password_list)
     password_entry.insert(0, password)
-    pyperclip.copy(password) #copy the password automatically and ready to paste in the website.
+    pyperclip.copy(password)  # copy the password automatically and ready to paste in the website.
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save():
-
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
-        messagebox.showinfo(title="Oops", message = "Please make sure you haven't left any field empty")
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any field empty")
 
     else:
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
 
-    # message box to ask user to confirm the info
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail:{email}"
-                               f"\nPassword: {password} \nIs it ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website}| {email}| {password} \n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+
+        else:
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+
+def find_password():
+    website = website_entry.get()
+    print(website)
+    with open("data.json") as data_file:
+        data = json.load(data_file)
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\n Password: {password}")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -74,19 +105,20 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
-password_entry = Entry(width=35)
-password_entry.grid(row=3, column=1, columnspan=2)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_password_button = Button(text="Generate Password", width=10, command = generate_password)
+search_button = Button(text="Search", width=10, command=find_password)
+search_button.grid(row=1, column=2)
+generate_password_button = Button(text="Generate Password", width=10, command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=33, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
-
 
 window.mainloop()
