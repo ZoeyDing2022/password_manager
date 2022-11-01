5fe3e31ff6bd8136d303f5d4101553f2be79aa15
ZoeyDing2022 <dzhaolan@hotmail.com>
2022-10-31 23:05:08 -0700

    Json file is used to store the information. search button is added so that you can search the email used to register and password of the website.

--- main.py
+++ main.py
@@ -84,6 +84,9 @@ def find_password():
             password = data[website]["password"]
             messagebox.showinfo(title=website, message=f"Email: {email}\n Password: {password}")
 
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
